8ca5ec6d2f09001d6496bf91d08710b2973aab65
robert-oppenheimer <ulusoyoguzhan@gmail.com>
2019-12-23 18:48:23 +0300

    resnet50

---
+++ resnet50.py
@@ -0,0 +1,19 @@
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
+
+'''
+weights can be changed with another data set
+'''
+model = ResNet50(weights='imagenet')
+
+img_path = 'Users//oppenheimer//Desktop//images//osman.jpeg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+
+print('Predicted:', decode_predictions(preds, top=3)[0])
